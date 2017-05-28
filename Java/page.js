df86c9a224cd3ce89e0423beac53c27785bd1a64
ronipeter <ronipedroso@Ronis.local>
2017-05-28 16:22:53 +0200

    all

---
+++ Java/page.js
@@ -0,0 +1,620 @@
+/*Photo Click David Fielden*/
+	$(document).ready(function(){
+	$(".dav").click(function(){
+	$("#dadata").fadeIn('slow'); 
+	$(".je, .yo, .ro, .cy, .ol, .pr").click(function(){
+	$("#dadata").fadeOut(300); 
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".dav1").click(function(){
+	$(".dav11").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".dav11").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".dav2").click(function(){
+	$(".dav22").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".dav22").fadeOut();
+	}
+	});
+	});
+	});
+ 	$(document).ready(function(){
+	$(".dav3").click(function(){
+	$(".dav33").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".dav33").fadeOut();
+	}
+	});
+	});
+	}); 
+	$(document).ready(function(){
+	$(".dav4").click(function(){
+	$(".dav44").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".dav44").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".dav5").click(function(){
+	$(".dav55").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".dav55").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".dav6").click(function(){
+	$(".dav66").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".dav66").fadeOut();
+	}
+	});
+	});
+	});
+
+ /*Photo Click Jesus Peiro*/
+	$(document).ready(function(){
+	$(".je").click(function(){
+	$("#dadata2").fadeIn('slow'); 
+	$(".dav, .yo, .ro, .cy, .ol, .pr").click(function(){
+	$("#dadata2").fadeOut(300); 
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".je1").click(function(){
+	$(".je11").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".je11").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".je2").click(function(){
+	$(".je22").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".je22").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".je3").click(function(){
+	$(".je33").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".je33").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".je4").click(function(){
+	$(".je44").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".je44").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".je5").click(function(){
+	$(".je55").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".je55").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".je6").click(function(){
+	$(".je66").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".je66").fadeOut();
+	}
+	});
+	});
+	});
+
+/*Photo Click Yolanda Cris*/
+	$(document).ready(function(){
+	$(".yo").click(function(){
+	$("#dadata3").fadeIn('slow'); 
+	$(".dav, .je, .ro, .cy, .ol, .pr").click(function(){
+	$("#dadata3").fadeOut(300); 
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".yo1").click(function(){
+	$(".yo11").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".yo11").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".yo2").click(function(){
+	$(".yo22").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".yo22").fadeOut();
+	}
+	});
+	});
+	});
+ 	$(document).ready(function(){
+	$(".yo3").click(function(){
+	$(".yo33").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".yo33").fadeOut();
+	}
+	});
+	});
+	}); 
+	$(document).ready(function(){
+	$(".yo4").click(function(){
+	$(".yo44").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".yo44").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".yo5").click(function(){
+	$(".yo55").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".yo55").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".yo6").click(function(){
+	$(".yo66").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".yo66").fadeOut();
+	}
+	});
+	});
+	});
+
+/*Photo Click Rosa Clara*/
+	$(document).ready(function(){
+	$(".ro").click(function(){
+	$("#dadata4").fadeIn('slow'); 
+	$(".dav, .je, .yo, .cy, .ol, .pr").click(function(){
+	$("#dadata4").fadeOut(300); 
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".ro1").click(function(){
+	$(".ro11").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ro11").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".ro2").click(function(){
+	$(".ro22").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ro22").fadeOut();
+	}
+	});
+	});
+	});
+ 	$(document).ready(function(){
+	$(".ro3").click(function(){
+	$(".ro33").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ro33").fadeOut();
+	}
+	});
+	});
+	}); 
+	$(document).ready(function(){
+	$(".ro4").click(function(){
+	$(".ro44").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ro44").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".ro5").click(function(){
+	$(".ro55").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ro55").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".ro6").click(function(){
+	$(".ro66").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ro66").fadeOut();
+	}
+	});
+	});
+	});
+
+/*Photo Click Cymbeline*/
+	$(document).ready(function(){
+	$(".cy").click(function(){
+	$("#dadata5").fadeIn('slow'); 
+	$(".dav, .je, .yo, .ro, .ol, .pr").click(function(){
+	$("#dadata5").fadeOut(300); 
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".cy1").click(function(){
+	$(".cy11").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".cy11").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".cy2").click(function(){
+	$(".cy22").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".cy22").fadeOut();
+	}
+	});
+	});
+	});
+ 	$(document).ready(function(){
+	$(".cy3").click(function(){
+	$(".cy33").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".cy33").fadeOut();
+	}
+	});
+	});
+	}); 
+	$(document).ready(function(){
+	$(".cy4").click(function(){
+	$(".cy44").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".cy44").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".cy5").click(function(){
+	$(".cy55").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".cy55").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".cy6").click(function(){
+	$(".cy66").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".cy66").fadeOut();
+	}
+	});
+	});
+	});
+
+/*Photo Click Olvis*/
+	$(document).ready(function(){
+	$(".ol").click(function(){
+	$("#dadata6").fadeIn('slow'); 
+	$(".dav, .je, .yo, .ro, .cy, .pr").click(function(){
+	$("#dadata6").fadeOut(300); 
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".ol1").click(function(){
+	$(".ol11").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ol11").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".ol2").click(function(){
+	$(".ol22").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ol22").fadeOut();
+	}
+	});
+	});
+	});
+ 	$(document).ready(function(){
+	$(".ol3").click(function(){
+	$(".ol33").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ol33").fadeOut();
+	}
+	});
+	});
+	}); 
+	$(document).ready(function(){
+	$(".ol4").click(function(){
+	$(".ol44").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ol44").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".ol5").click(function(){
+	$(".ol55").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ol55").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".ol6").click(function(){
+	$(".ol66").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".ol66").fadeOut();
+	}
+	});
+	});
+	});
+
+	/*Photo Click Pronovias*/
+	$(document).ready(function(){
+	$(".pr").click(function(){
+	$("#dadata7").fadeIn('slow'); 
+	$(".dav,.je, .yo, .ro, .cy, .ol").click(function(){
+	$("#dadata7").fadeOut(300); 
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".pr1").click(function(){
+	$(".pr11").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".pr11").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".pr2").click(function(){
+	$(".pr22").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".pr22").fadeOut();
+	}
+	});
+	});
+	});
+ 	$(document).ready(function(){
+	$(".pr3").click(function(){
+	$(".pr33").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".pr33").fadeOut();
+	}
+	});
+	});
+	}); 
+	$(document).ready(function(){
+	$(".pr4").click(function(){
+	$(".pr44").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".pr44").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".pr5").click(function(){
+	$(".pr55").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".pr55").fadeOut();
+	}
+	});
+	});
+	});
+	$(document).ready(function(){
+	$(".pr6").click(function(){
+	$(".pr66").show(); 
+	$(".back").css({'background-color': 'black', 'width': '100vw', 'height': '100vh', 'position': 'absolute', 'z-index':'3','opacity':'0.8','display':'block'});
+	$(".back").click(function(e) {
+	e.preventDefault();
+	if(e.target == this){
+	$(".back").fadeOut();
+	$(".pr66").fadeOut();
+	}
+	});
+	});
+	});
+
+
+
+
+
